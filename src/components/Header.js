d8094109050037cc7d7360a50f88d3166937a6b3
kaseen <srbtrxtheinsane@gmail.com>
2023-12-16 18:35:39 +0100

    Header reworked

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
+import { Box, Button } from '@mui/material';
+import { styled } from '@mui/system';
 
 export const Header = () => {
 
@@ -9,19 +11,49 @@ export const Header = () => {
     });
     const { disconnect } = useDisconnect();
 
+    const Container = styled(Box)({
+        padding: '25px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF'
+    });
 
-    // TODO
-    if(isConnected)
-        return (
-            <>
-                Connected to {(address.slice(0,4) + '...' + address.slice(38))}
-                <button onClick={() => disconnect()}>Disconnect</button>
-            </>
-        )
-    else
-        return (
-            <>
-                <button onClick={() => connect()}>Connect Wallet</button>
-            </>
-        )
+    const ButtonStyled = styled(Button)({
+        backgroundColor: '#F8F8F8',
+        borderRadius: '5px',
+    });
+
+    const ContainerItem = styled(Box)({
+        minWidth: '20%'
+    })
+
+    return (
+        <Container>
+            <ContainerItem sx={{
+                fontSize: '100%',
+            }}>
+                {isConnected && <>Connected to: {(address.slice(0,5) + '...' + address.slice(38))}</>}
+            </ContainerItem>
+
+            <ContainerItem sx={{ 
+                fontSize: '170%',
+                fontWeight: '1000',
+                textAlign: 'center'
+            }}>
+                LUCKY SIX
+            </ContainerItem>
+
+            <ContainerItem sx={{
+                textAlign: 'right'
+            }}>
+                {isConnected ? 
+                    <ButtonStyled onClick={() => disconnect()}>Disconnect</ButtonStyled>
+                        :
+                    <ButtonStyled onClick={() => connect()}>Connect Wallet</ButtonStyled>
+                }
+            </ContainerItem>
+
+        </Container>
+    )
 }
